7176bf5ce254635d56063567168d07f051a96e48
Mobina Esterabi <esterabimobina@gmail.com>
2023-11-11 12:54:51 -0500

    nav completed.

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,8 +2,15 @@
 module.exports = {
   content: ["./*.html"],
   theme: {
+    screens: {
+      sm: "640px",
+      md: "860px",
+      lg: "1024px",
+      xl: "1280px",
+      xbox: "878px",
+      deals: "927px",
+    },
     extend: {},
   },
   plugins: [],
-}
-
+};
