4c1805c6622cbfda768a6757a80370b84608442b
Mobina Esterabi <esterabimobina@gmail.com>
2023-11-18 15:06:22 -0500

    swiper scripts added.

---
+++ main.js
@@ -0,0 +1,10 @@
+var swiper = new Swiper(".mySwiper", {
+  /*autoplay: {
+        delay: 3000,
+        disableOnInteraction: false,
+    },*/
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+  },
+});
